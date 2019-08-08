97c29a1dfe5e85a440222848161c3342aa6f24ab
vasiliv <ggo24@yahoo.com>
2019-08-08 11:15:21 +0400

    Add project files.

---
+++ TemplatedHtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplatedHtmlHelper.Models;
+
+namespace TemplatedHtmlHelper.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            Employee emp = new Employee() {
+                Id = 1,
+                Email = "Georgia@gmail.com",
+                Name = "Vasil",
+                IsEmployee = true,
+                DateOfBirth = DateTime.Now
+            };
+
+            return View(emp);
+        }
+        public ActionResult Edit() {            
+            return View();
+        }
+        [HttpPost]
+        //Press me buttonis dacheris shemdeg aq shemova monacemebi Employee emp-is saxit
+        public ActionResult Edit(Employee emp) {
+            return View(emp);
+        }
+    }
+}
---
+++ TemplatedHtmlHelper/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TemplatedHtmlHelper.Models {
+    public class Employee {
+        public int Id { get; set; }
+        [Display(Name = "Please enter your name: ")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsEmployee { get; set; }
+        [Display(Name = "Date of birth: ")]
+        //svavs kalendars
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        //svavs saatis chasacers
+        [DataType(DataType.Time)]
+        public DateTime BirthTime { get; set; }
+    }
+}
