2795a326c6ac46e2f8b28379ce41f4b48f5e1676
vasiliv <ggo24@yahoo.com>
2019-08-08 10:20:15 +0400

    Add project files.

---
+++ TemplatedHtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplatedHtmlHelper.Models;
+
+namespace TemplatedHtmlHelper.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            Employee emp = new Employee() {
+                Id = 1,
+                Email = "Georgia@gmail.com",
+                Name = "Vasil",
+                IsEmployee = true,
+                DateOfBirth = DateTime.Now
+            };
+
+            return View(emp);
+        }
+    }
+}
---
+++ TemplatedHtmlHelper/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TemplatedHtmlHelper.Models {
+    public class Employee {
+        public int Id { get; set; }
+        [Display(Name = "Please enter your name: ")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsEmployee { get; set; }
+        [Display(Name = "Date of birth: ")]
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
